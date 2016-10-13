302be2cb19d10040a565d048543a58330f00404b
Daniel Dugovic <dandydand@gmail.com>
2016-10-13 07:34:34 -0500

    Upload results to scoreboard and remove magic numbers

--- hillclimbing.py
+++ hillclimbing.py
@@ -6,7 +6,7 @@ def run_episode(env, parameters):
 	totalreward = 0
 	#for 200 timesteps
 	for _ in xrange(200):
-		env.render()
+		#env.render()
 		#initalize random weights
 		action = 0 if np.matmul(parameters, observation)<0 else 1
 		observation, reward, done, info = env.step(action)
@@ -17,23 +17,33 @@ def run_episode(env, parameters):
 
 #hill climbing algo training
 def train(submit):
+	np.random.seed(0)
+	outdir = '/tmp/gym/hill-climbing-agent-results'
 	env = gym.make('CartPole-v0')
+	env.monitor.start(outdir, force=True, seed=0)
 
 	episodes_per_update = 5
 	noise_scaling = 0.1
-	parameters = np.random.rand(4) * 2 -1
+	parameters = np.zeros(env.observation_space.shape[0])
 	bestreward = 0
 
-	#2000 episodes
-	for _ in xrange(2000):
-		newparams = parameters + (np.random.rand(4) *2 -1) * noise_scaling
+	#100 episodes
+	for x in xrange(100):
+		newparams = parameters + (np.random.rand(len(parameters)) * 2 - 1) * noise_scaling
 		reward = run_episode(env, newparams)
 		print "reward %d best %d" % (reward, bestreward)
 		if reward > bestreward: 
 			bestreward = reward
 			parameters = newparams
 			if reward == 200:
-				break
+				noise_scaling /= 2.0
 
-r = train(submit=False)
+	# Dump result info to disk
+	env.monitor.close()
+
+	# Upload to the scoreboard
+	if submit:
+		gym.upload(outdir)
+
+r = train(submit=True)
 print r
